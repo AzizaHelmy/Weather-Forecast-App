c8a900efa599e3c9090e794fd39a82d817984ea2
Aziza Helmy <aziza.helmy35@gmail.com>
2022-03-06 00:30:47 -0700

    adding bottom navigation

--- app/src/main/java/com/example/weatherforecastapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecastapp/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.weatherforecastapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.weatherforecastapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.favFragment,
+                R.id.alertsFragment,
+                R.id.settingsFragment))
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+//        val navController = navHostFragment.navController
+//        NavigationUI.setupWithNavController(binding.bottomNav,navController)
+
     }
 }
---
+++ app/src/main/java/com/example/weatherforecastapp/settings/SettingsFragment.kt
@@ -0,0 +1,12 @@
+package com.example.weatherforecastapp.settings
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import com.example.weatherforecastapp.R
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+}
